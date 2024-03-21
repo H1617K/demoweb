484175b09c25d8c7f7d53eab984d681873b8a430
harsh <harsh@WHOHARSH>
2024-03-21 11:50:34 +0530

    Add some dipendcy and navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from './Product/product';
+import Navbar from './Navbar/Navbar';
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Navbar/>
       <Routes>
         <Route path='/Product' element={<Product/>}></Route>
       </Routes>
